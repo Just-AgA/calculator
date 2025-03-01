e35078e9ea0e02891efff00eb3a213f952d4c078
Just-AgA <161239277+Just-AgA@users.noreply.github.com>
2025-03-01 11:36:29 +0100

    Add function that calls one of the main function
    Added the function that calls one of the main functions on the operands,based on the operator

--- script.js
+++ script.js
@@ -1,27 +1,39 @@
 function addNumbers(num1, num2){
-    return num1 + num2;
+    return Number(num1) + Number(num2);
 }
 
 function subtractNumbers(num1, num2){
-    return num1 - num2;
+    return Number(num1) - Number(num2);
 }
 
 function multiplyNumbers(num1, num2){
-    return num1 * num2;
+    return Number(num1) * Number(num2);
 }
 
 function divideNumbers(num1, num2){
-    if(num2 === 0){
+    if(Number(num2) === 0){
         return "Cannot divide by 0";
     }
 
-    return num1 / num2;
+    return Number(num1) / Number(num2);
+}
+
+// Create function so it performs a calculation based on the operator
+function operate(num1, operator, num2){
+    if(operator == "+"){
+        addNumbers(num1, num2);
+    } else if(operator == "-"){
+        subtractNumbers(num1, num2);
+    }else if(operator == "*"){
+        multiplyNumbers(num1, num2);
+    }else if(operator == "/"){
+        divideNumbers(num1, num2);
+    }
 }
 
 // Create variables for the selectors and operators & operands
 const display = document.querySelector(".display");
-const squareView = document.querySelector(".square-view");
-const operators = document.querySelector(".operators");
-let leftOperand;
-let rightOperand;
-let operator;
+const digits = document.querySelectorAll(".digit");
+const dot = document.querySelector("#dot");
+
+
